73a59862c7ae09dda706d5ce1892d2f106cdd6cf
Chet Woodside <chet@cosmicblend.ca>
2013-04-10 12:02:47 -0700

    WIP AND ROLL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wip::Application.config.secret_token = '9a37d6e9f835e921f364c353a271dde473c7d574770a495a4e9aecee98ec083b13497380e181ace2c00320d86475a787fc389da778bf8cd92c81c618e07bfe35'
