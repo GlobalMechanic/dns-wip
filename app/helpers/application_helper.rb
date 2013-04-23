f6d2a4943452234457b8772099c53a673c705275
Tylor Sherman <tylor@denimandsteel.com>
2013-04-23 14:45:01 -0700

    Order page assets and make submit and keyup binding live

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def is_image(asset)
+    %w(jpg jpeg gif png).include?(clean_extension(asset))
+  end
+
+  def is_movie(asset)
+    %w(mov mp4).include?(clean_extension(asset))
+  end
+
+  def clean_extension(filename)
+    extension = File.extname(filename.to_s).downcase
+    extension = extension[1..-1] if extension[0,1] == '.'
+  end
 end
